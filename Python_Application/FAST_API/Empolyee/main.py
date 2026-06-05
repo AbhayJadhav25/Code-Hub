04182d99b127ce49f62b7be7118774179b859c80
Abhi-205 <abhayjadhav205@gmail.com>
2026-06-06 00:38:51 +0530

    Fully CRUD API's mini Project

--- Python_Application/FAST_API/Empolyee/main.py
+++ Python_Application/FAST_API/Empolyee/main.py
@@ -25,6 +25,12 @@ class CreateEmployee(BaseModel):
     department : str
     salary : float
 
+class UpdateEmployee(BaseModel):
+    name : Optional[str] = None
+    email : Optional[EmailStr] = None
+    department : Optional[str] = None
+    salary : Optional[float] = None
+
 @employee_router.get("")
 def root():
     return{
@@ -75,6 +81,53 @@ def view_emp_with_id(emp_id : int):
         status_code=404 , 
         detail="Employee not found."
     )
+
+@employee_router.put('/update/{emp_id}')
+def update_emp(emp_id : int , emp : UpdateEmployee):
+    data = load_data()
+    existing_info = {}
+    update_info = {}
+
+    for e in data :
+        if e['id'] == emp_id:
+            
+            update_info  = emp.model_dump(exclude_unset = True)
+
+            for key , value in update_info.items():
+                e[key] = value
+
+            save_data(data)
+
+            return JSONResponse(
+                status_code=200 , 
+                content={'message' : 'Updated Successfully'}
+            )
+    
+    raise HTTPException(
+        status_code=404 ,
+        detail='Employee Not Found'
+    )
+
+@employee_router.delete('/delete/{emp_id}')
+def delete(emp_id : int ):
+    data = load_data()
+
+    for e in data:
+        if e['id'] == emp_id :
+            data.remove(e)
+
+            save_data(data)
+            return JSONResponse(
+                status_code=200 , 
+                content={'message' : 'Employee deleted Successfully'}
+            )
+        
+    
+    raise HTTPException(
+        status_code=404 , 
+        detail='Employee Not Found'
+    )
+
 
 app.include_router(employee_router)
 
